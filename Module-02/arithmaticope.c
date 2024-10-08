f8376aa1a17599ffb4db289ab9b8345fffb84ca8
masdrana1221 <masud.mr5622@gmail.com>
2024-10-08 23:16:59 +0800

    Add Module 2

---
+++ Module-02/arithmaticope.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int a;
+   int b;
+   int c;
+   scanf("%d %d %d", &a , &b , &c);
+   printf("%d", a - b * c);
+   return 0;
+}
+
+/* Those are Additional Operators.
+   try to some practices with using these operators.
+   + -> Addition
+   - -> Subtraction
+   * -> Multiplication
+   / -> Division
+   % -> Modulus
+*/
---
+++ Module-02/assignmentop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int a;
+   printf("Input a Number:");
+   scanf("%d", &a);
+   a += 10;
+   printf("%d", a); // Output : 21
+   return 0;
+}
+
+/*
+This is a Assignment Operator which are add value of int a
+$how have been made these?
+a=a+5 => a+=5
+*/
